4d6c9f56c3743ad5cfacef0996b2fcd48f6b8a7f
Conner Lane <cal0043@auburn.edu>
2017-12-04 00:29:39 -0600

    clean up comments

--- wiki_link.py
+++ wiki_link.py
@@ -46,7 +46,7 @@ def get_links_from_page(url):
         for link in list.findAll('a'):
             if link_is_valid(link):
                 links.append(link)
-    return links
+    return [(a.get('href')[6:], a.contents[0]) for a in links]
 
 
 def load_cache():
@@ -60,17 +60,6 @@ def write_cache(table):
     with open(CACHE_FILE, 'w') as file:
         file.write(json.dumps(table))
 
-# def load_table():
-#     table_file = Path("table.json")
-#     if table_file.exists():
-#         return json.load(open("table.json"))
-#     return dict()
-
-
-# def write_table(table):
-#     with open("table.json", 'w') as file:
-#         file.write(json.dumps(table))
-
 
 def build_path(current):
     path = []
@@ -82,29 +71,28 @@ def build_path(current):
     return path
 
 
-def relate(start, destination):
+def relate(start, destination, grow_cache=False):
     visited = set()
     cache = load_cache()
     q = Queue()
     # add initial state with no parent
     q.put(Page(strip_url(start), strip_url(start), None))
-    visited.add(strip_url(start))
     goal_suffix = strip_url(destination)
     while True:
         page = q.get()
         if page.url in cache:
-            print("*** ", end="")
+            print("* ", end="")
             links = cache[page.url]
         else:
-            anchor_tags = get_links_from_page(
+            links = get_links_from_page(
                 "https://en.wikipedia.org/wiki/" + page.url)
-            links = [(a.get('href')[6:], a.contents[0]) for a in anchor_tags]
-            # cache[page.url] = links
-        print(build_path(page))
+            if grow_cache:
+                cache[page.url] = links
+        print(" -> ".join(build_path(page)))
         for url, title in links:
             if url not in visited:
                 if url == goal_suffix:
-                    # write_cache(cache)
+                    if grow_cache: write_cache(cache)
                     p = build_path(page)
                     p.append(title)
                     return p
@@ -113,5 +101,11 @@ def relate(start, destination):
 
 
 if __name__ == "__main__":
-    print(" -> ".join(relate("https://en.wikipedia.org/wiki/Jasper_High_School_(Alabama)",
-                             "https://en.wikipedia.org/wiki/DYWP")))
+    start_link = input("starting page: ")
+    if input("custom destination page? [Y/N]: ").strip().upper() == "Y":
+        dest_link = input("destination page: ")
+    else:
+        dest_link = "https://en.wikipedia.org/wiki/Adolf_Hitler"
+    grow_cache = True if input(
+        "grow cache? [Y/N]: ").strip().upper() == "Y" else False
+    print(" -> ".join(relate(start_link, dest_link, grow_cache=grow_cache)))
